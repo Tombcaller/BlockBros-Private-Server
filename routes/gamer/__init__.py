94f8969db641a4f96c14383a2703d44121a01a95
Tombcaller <tombcaller@gmail.com>
2025-10-07 21:23:27 +0100

    gamer/search

--- routes/gamer/__init__.py
+++ routes/gamer/__init__.py
@@ -1,6 +1,7 @@
 from .nickname.check import gamer_nickname_check_bp
 from .put import gamer_put_bp
+from .search import gamer_search_bp
 
-gamer_bps = [gamer_nickname_check_bp, gamer_put_bp]
+gamer_bps = [gamer_nickname_check_bp, gamer_put_bp, gamer_search_bp]
 
 __all__ = ["gamer_bps"]
--- routes/gamer/search.py
+++ routes/gamer/search.py
@@ -1,10 +1,57 @@
-from flask import Blueprint, request, Response
+#§ -------- IMPORTS -------- §#
+#§ SQLAlchemy Imports §#
+from sqlalchemy import func
+
+#§ Flask Imports §#
+from flask import Blueprint, request
+
+#§ Server Utility Imports §#
+from models import account
+from utils.response import generateResponse, checkToken, tokenMismatchResponse
+from utils.get_db_data import getPublicPlayerData
+
+#§ Misc Imports §#
 import time
-import json
-import gzip
+#§ ------------------------- §#
 
 gamer_search_bp = Blueprint("gamer_search", __name__, url_prefix="/gamer")
 @gamer_search_bp.route("/search", methods=["POST"])
 
 def search():
-    return
+    #§ Checking token validity §#
+    if checkToken(request.headers.get("Authorization")) == False:
+        return tokenMismatchResponse()
+    else:
+        loggedInId = request.headers.get("Authorization").split(":")[0]
+
+    #§ Getting user's request data from Flask §#
+    request_data = request.get_json()
+
+    #§ Defining params to check in DB from user's request data §#
+    nicknameToCheck = request_data.get("nickname")
+
+    #§ Checking if request contains required paramaters §#
+    if not nicknameToCheck:
+        return {"error": "Invalid request"}, 400
+    
+    #§ Looking up nickname in database §#
+    accountToReturn = account.query.filter(func.lower(account.nickname) == nicknameToCheck.lower()).first()
+    if accountToReturn is None:
+        success = False
+    else:
+        success = True
+
+    #§ Creating body to send §#
+    body = {
+        "success": success,
+        "result": {
+            "all_loaded": True,
+            "index": 1 if success else 0,
+            "items": [getPublicPlayerData(accountToReturn.internalId)] if success == True else [],
+        },
+        "updated": {},
+        "timestamp": int(time.time())
+        }
+
+    #§ Use utils.response generateResponse to format correctly (GZip + Headers) §#
+    return generateResponse(body)
