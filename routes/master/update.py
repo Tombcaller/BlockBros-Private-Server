bbaf90a392b2fb09b5fd2f29d615e87c74cd9420
Tombcaller <tombcaller@gmail.com>
2026-05-01 17:19:47 +0100

    Fix master/update config

--- routes/master/update.py
+++ routes/master/update.py
@@ -4,18 +4,13 @@ from flask import Blueprint
 
 #§ Server Utility Imports §#
 from utils.response import generateResponse
+from config.config import mainConfig
 
 #§ Misc Imports §#
 import time
-import json
-from pathlib import Path
 #§ ------------------------- §#
 
 master_update_bp = Blueprint("master_update", __name__, url_prefix="/master")
-
-config_path = Path(__file__).resolve().parents[2] / "config" / "config.json"
-with open(config_path, "r", encoding="utf-8") as f:
-    config = json.load(f)
 
 @master_update_bp.route("/update", methods=["POST"])
 def update():
@@ -29,10 +24,10 @@ def update():
             "timestamp": int(time.time())
             }
     
-    if config["versionLoading"]["enabled"]:
+    if mainConfig["versionLoading"]["enabled"]:
         body["master"] = {
             "config":{
-                "assetversion": config["versionLoading"]["version"]
+                "assetversion": mainConfig["versionLoading"]["version"]
                 }
             }
                 
