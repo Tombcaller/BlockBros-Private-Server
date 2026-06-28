c15d746fffb987dadadde9b7da6b2e907f91f7ba
Tombcaller <tombcaller@gmail.com>
2026-06-28 02:17:46 +0100

    emblem ownlist bp

--- routes/emblem/__init__.py
+++ routes/emblem/__init__.py
@@ -1,6 +1,7 @@
 from .get import emblem_get_bp
 from .post import emblem_post_bp
+from .ownList import emblem_own_list_bp
 
-emblem_bps = [emblem_get_bp, emblem_post_bp]
+emblem_bps = [emblem_get_bp, emblem_post_bp, emblem_own_list_bp]
 
 __all__ = ["emblem_bps"]
